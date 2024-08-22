59318d723e06917440f909ff6569c8e91f9b8fe2
CarlosAugustoP <capv2004@gmail.com>
2024-08-22 09:12:09 -0300

    undo

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../app/globals.css";
 
 export const ImageContainer = (props) => {
@@ -10,37 +10,32 @@ export const ImageContainer = (props) => {
 }
 
 export function About() {
-    const [isLoading, setIsLoading] = useState(true);
+    const [text, setText] = useState('');
+    const [showImg, setShowImg] = useState(false);
 
-    const handleImageLoad = () => {
-        setIsLoading(false);
-    };
-
-    const handleImageError = () => {
-        setIsLoading(false); // You might want to handle this differently if there's an error
-    };
+    useEffect(() => {
+        setTimeout(() => {
+            setText("I waited 2 seconds to be loaded!");
+            setShowImg(true);
+        }, 2000);
+    }, []);
 
     return (
         <div className="flex gap-10">
             <div className="text-left w-1/2 flex flex-col justify-between">
                 <h1 className="text-[6.6vw] font-bold leading-none text-white">Hello World!</h1>
                 <p className="text-[1.55vw] text-white">My name is Carlos, and I'm a Computer Science student and FullStack Dev from Recife, Brazil, currently expanding my skills in React Native, Next.js and generative A.I! See some of my projects, learn more about me or get in contact!</p>
+                <p className="text-[1.55vw] text-gray-400">{text}</p>
             </div>
             <div className="w-1/2 relative">
-                {isLoading && (
+                {showImg ? (
+                    <img src='/img/picture.jpeg' alt='Carlos Augusto' className='w-full h-auto rounded-lg border-white border-4' />
+                ) : (
                     <div className="absolute inset-0 flex items-center justify-center bg-gray-300 rounded-lg border-white border-4">
-                        {/* Spinner */}
+                        {/* This is a spinner or skeleton placeholder */}
                         <div className="w-16 h-16 border-4 border-t-4 border-t-transparent border-white rounded-full animate-spin"></div>
                     </div>
                 )}
-                <img
-                    src='/img/picture.jpeg'
-                    alt='Carlos Augusto'
-                    className='w-full h-auto rounded-lg border-white border-4'
-                    onLoad={handleImageLoad}
-                    onError={handleImageError}
-                    style={{ display: isLoading ? 'none' : 'block' }}
-                />
             </div>
         </div>
     );
