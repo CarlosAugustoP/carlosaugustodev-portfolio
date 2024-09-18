a59f9f8490fb81ff6600892715215a2df0243067
CarlosAugustoP <capv2004@gmail.com>
2024-09-18 20:51:30 -0300

    adding next images

--- src/components/container.js
+++ src/components/container.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import Image from 'next/image';
 import "../app/globals.css";
 
-export default function AboutContainer({ header, text, img, direction}) {
+export default function AboutContainer({ header, text, img, direction }) {
     const [isMobile, setIsMobile] = useState(false);
+
     useEffect(() => {
         const checkMobile = () => {
             setIsMobile(window.innerWidth < 1024);
@@ -19,22 +21,39 @@ export default function AboutContainer({ header, text, img, direction}) {
             window.removeEventListener("resize", checkMobile);
         };
     }, []);
+
     if (isMobile) {
         return (
             <div className="flex flex-col gap-5">
                 <h1 className="text-10xl font-bold leading-none text-white">{header}</h1>
                 <p className="text-white">{text}</p>
-                <img src={img} className="rounded-lg border-white border-4 " alt="Carlos Augusto" />
+                <div className="relative w-full h-64">
+                    <Image 
+                        src={img} 
+                        alt="Carlos Augusto" 
+                        layout="fill"
+                        className="rounded-lg border-white border-4 object-cover"
+                    />
+                </div>
             </div>
         );
     }
+
     return (
         <div className={`flex gap-10 ${direction}`}>
             <div className="text-left w-1/2 flex flex-col justify-between">
                 <h1 className="text-[6.6vw] font-bold leading-none text-white">{header}</h1>
                 <p className="text-[1.55vw] text-white">{text}</p>
             </div>
-                <img src={img} className='w-1/2 rounded-lg border-white border-4' alt='Carlos Augusto' />
+            <div className="relative w-1/2 h-auto">
+                <Image 
+                    src={img} 
+                    alt="Carlos Augusto" 
+                    layout="fill"
+                    className="rounded-lg border-white border-4 object-cover"
+                    loading="lazy"
+                />
+            </div>
         </div>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import '../app/globals.css';
 import Navbar from '../components/navbar';
 import { About, Hr } from '@/components/about';
 import AboutContainer from '@/components/container';
-import { useEffect, useState } from 'react';
 
-export default function home() {
+export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -28,8 +28,30 @@ export default function home() {
   return (
     <div className='bg-gradient-to-b from-black to-blue-400 flex flex-col items-center justify-center gap-40'>
       <Navbar className={'absolute'} />
-      <img className={isMobile ? 'hidden' : 'absolute h-56 right-0 bottom-24 rotate-12'} src='img/neural.svg' />
-      <img className={isMobile ? 'hidden' : 'absolute h-56 left-0 top-24 rotate-12'} src='img/code.svg' />
+      {!isMobile && (
+        <>
+          <div className='absolute h-56 right-0 bottom-24 rotate-12'>
+            <Image 
+              src='/img/neural.svg' 
+              alt='Neural'
+              width={224}
+              height={224}
+              priority={true}
+              quality={50}
+            />
+          </div>
+          <div className='absolute h-56 left-0 top-24 rotate-12'>
+            <Image 
+              src='/img/code.svg' 
+              alt='Code'
+              width={224}
+              height={224}
+              priority={true}
+              quality={50}
+            />
+          </div>
+        </>
+      )}
       <div className='h-[100vh] w-full flex items-center justify-center'>
         <div className='w-full flex items-center justify-center'>
           <div className={isMobile ? 'flex gap-16 w-4/5' : 'flex gap-16 w-3/5'}>
@@ -39,25 +61,50 @@ export default function home() {
       </div>
       <div className='w-full flex items-center justify-center'>
         <div id="About" className={isMobile ? 'flex gap-16 w-4/5' : 'flex gap-16 w-3/5'}>
-          <AboutContainer header='CESAR school' text='I study Computer Science at CESAR School, which prioritizes innovation and teamwork and directly integrates us with Porto Digital, one of the biggest digital centers in South America. I am currently in my 5th semester!' img='img/cesar.png' direction='flex-row-reverse' />
+          <AboutContainer header='CESAR school' text='I study Computer Science at CESAR School, which prioritizes innovation and teamwork and directly integrates us with Porto Digital, one of the biggest digital centers in South America. I am currently in my 5th semester!' img='/img/cesar.png' direction='flex-row-reverse' />
         </div>
       </div>
       <div className='w-full flex items-center justify-center'>
         <div className={isMobile ? 'flex gap-16 w-4/5' : 'flex gap-16 w-3/5'}>
-          <AboutContainer header='About Me (:' text="Ever since i was little, i've been in love with computers. Whether it's the internet, games, hardware or software, working with Computer Science is a dream come true! In my free time, i love playing games, drawing, hitting the gym and travelling!" img='img/viagem.jpeg' />
+          <AboutContainer header='About Me (:' text="Ever since i was little, i've been in love with computers. Whether it's the internet, games, hardware or software, working with Computer Science is a dream come true! In my free time, i love playing games, drawing, hitting the gym and travelling!" img='/img/viagem.jpeg' />
         </div>
       </div>
-      <div id = "Contact" className='w-full flex flex-col items-center justify-center gap-3'>
+      <div id="Contact" className='w-full flex flex-col items-center justify-center gap-3'>
         <h1 className='text-4xl font-bold text-white'>Contact me!</h1>
         <hr className='w-1/2 bg-white' />
         <div className='flex gap-4 items-center mb-10'>
-          <a href="mailto:capv2004@gmail.com?subject=Portfolio%20Interest&body=Hi%20Carlos%2C%0D%0A%0D%0AI'm%20interested%20in%20discussing%20your%20portfolio.%20Let's%20connect!%0D%0A%0D%0AThanks%2C%0D%0A[Your%20Name]">
-            <img src="img/gmail.svg" className="h-10 w-10" />
+          <a href="mailto:capv2004@gmail.com?subject=Portfolio%20Interest&body=Hi%20Carlos%2C%0D%0A%0D%0AI'm%20interested%in%20discussing%20your%20portfolio.%20Let's%20connect!%0D%0A%0D%0AThanks%2C%0D%0A[Your%20Name]">
+            <Image 
+              src='/img/gmail.svg' 
+              alt='Gmail'
+              width={40}
+              height={40}
+              quality={50}
+              loading="lazy"
+            />
           </a>
-          <a href='https://github.com/CarlosAugustoP'><img src='img/github.svg' className='h-10 w-10'></img></a>
-          <a href='https://www.linkedin.com/in/carlos-augusto-vasconcelos-6472a3240/'><img src='img/linkedin.svg' className='h-10 w-10'></img></a>
+          <a href='https://github.com/CarlosAugustoP'>
+            <Image 
+              src='/img/github.svg' 
+              alt='GitHub'
+              width={40}
+              height={40}
+              quality={50}
+              loading="lazy"
+            />
+          </a>
+          <a href='https://www.linkedin.com/in/carlos-augusto-vasconcelos-6472a3240/'>
+            <Image 
+              src='/img/linkedin.svg' 
+              alt='LinkedIn'
+              width={40}
+              height={40}
+              quality={50}
+              loading="lazy"
+            />
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
